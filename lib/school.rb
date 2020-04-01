78af581c2b57d9c8b48ebdf29c1ba71b65b6c688
Elias Taveras <eliastave12@gmail.com>
2020-04-01 05:00:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,9 +13,9 @@ class School
     @roster = roster_input
   end
   
-  def add_student(@name, number)
+  def add_student(name, number)
     if @roster[number]
-      @roster[number].push(@name)
+      @roster[number].push(name)
     else
       @roster[number] = []
   end
