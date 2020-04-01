769850edcd3b94b0fca29d91969437c4c15965c9
Elias Taveras <eliastave12@gmail.com>
2020-04-01 05:41:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster 
+  end
+  
+  def roster=(roster_input)
+    @roster = roster_input
+  end
+
+  def add_student(student_name, number)
+    if @roster[number]
+      @roster[number].push(student_name)
+    else
+      array = []
+      array << student_name
+      @roster[number] = array
+    end
+  end
+  
+  def grade(number)
+    @roster[number] 
+  end
+  
+  def sort
+    @roster.each do |k,v| 
+      v.sort!
+    end
+  end
+  
+end
+
+
